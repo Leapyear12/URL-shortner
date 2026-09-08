9d485268d4828d4f52437fc6ab0dcbdb2358b2f3
Leapyear12 <vinarmn786@gmail.com>
2026-09-09 00:01:14 +0530

    The backend of the URL shortener

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const urlRoute = require('./routes/url');
+const URL = require('./models/url');
+const { connectToMongoDB } = require('./connect');
+const PORT =8001;
+
+connectToMongoDB("mongodb://localhost:27017/url-short-url")
+.then(() => console.log("Connected to MongoDB"));
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({ 
+        shortId 
+    },
+    { 
+        $push: {
+           visitHistory: {timestamp: Date.now()},
+        },
+    });
+    res.redirect(entry.redirectUrl);
+});
+ 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
